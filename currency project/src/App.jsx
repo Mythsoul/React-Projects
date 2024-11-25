eeb6f06d2d1c7c59897c89d2080e493e0a38ba0a
Mythsoul <Mythsoul2023@gmail.com>
2024-11-25 19:44:43 +0545

    some code

--- currency project/src/App.jsx
+++ currency project/src/App.jsx
@@ -1,6 +1,10 @@
+import InputBox from "./components/InputBox"
 function App() {
 return(<>
-        <h1 className="text-center text-3xl">Hello </h1>
+        <h1 className="text-center text-3xl m-2">Currency Converter </h1>
+    <div>
+    <InputBox  /> 
+    </div>
     </>
 )
 }
